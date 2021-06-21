17f00f6d459d4b9fa33c83d1043e57ddeea90f5c
NitzanShahar <nitzansh@tauex.tau.ac.il>
2021-06-21 21:37:06 +0300

    fixing the simulation in the mmodeling folder

---
+++ modeling/Parameter_recovery_Hierarchical_fit_cov_matrix_cholesky.R
@@ -0,0 +1,185 @@
+#aim: Parameter recovery for the teacher-student paradigm with reliability estimates
+#contributor: Nitzan Shahar, Maayan Pereg
+
+rm(list=ls())
+library('rstan') # observe startup messages
+library("truncnorm")
+library(parallel)
+library(gtools) #inv.logit function 
+library(MASS)
+
+
+
+# generate population and subject level parameters -----------------------------------------------------------
+
+Nsubj =10         
+
+#population location parameters
+mu=c(
+alpha.inst.follow       =logit(0.4),
+alpha.inst.oppose       =logit(0.3),
+alpha.free              =logit(0.5),
+alpha.teacher_as_bandit =logit(0.5),
+alpha.reliability       =logit(0.5),
+beta                    =log(3),
+inst.bias               =0.2
+)
+Nparam=length(mu)
+
+#population scale parameters
+tau          =c(.2,.2,.2,.2,.2,.05,0.1) #var vector
+cov_param    =0
+sigma        = diag(tau)
+sigma[!diag(nrow=Nparam)]=cov_param
+
+# sample aux parameters
+auxiliary_parameters = mvrnorm(n = Nsubj, mu = mu, Sigma = sigma)
+
+
+
+# run a simulation study -----------------------------------------------------------
+
+source('modeling/sim_functions/simme_tsp.R')
+
+#main configuration variables for the simulation
+Nalt  =4         #number of alternatives
+Noffer=2         #how many cards from the deck are presented
+Ntrl  =300       #number of trials
+
+cfg=list(      Ntrl  =Ntrl,
+               Nalt  =Nalt,
+               Noffer=Noffer,
+               rndwlk_frac   =as.matrix(read.csv('modeling/sim_functions/rndwlk_4frc_1000trials.csv',header=F)),
+               rndwlk_teacher=rep(15,Ntrl),
+               teacher.rate=0.6)
+
+# simulating N agents 
+
+
+
+df<-lapply(1:Nsubj,function(s)   {cfg$subject=s
+                                     sim.block(par=auxiliary_parameters[s,],cfg)})
+
+df<-do.call(rbind,df)
+
+df%>%group_by(trial)%>%summarise(rcard=mean(Rcard))%>%plot()    
+df%>%group_by(trial)%>%summarise(rteacher=mean(Rteacher))%>%plot()    
+
+# parameter recovery with stan --------------------------------------------
+
+#prepare action and reward matrices (subject x trial)
+a1=t(sapply(1:Nsubj,function(subj) {df[df$subj==subj,'action']}))
+reward=t(sapply(1:Nsubj,function(subj) {df[df$subj==subj,'reward']}))
+
+#prepare data
+model_data <- list(Nsubj = Nsubj,
+                   Ntrials = Ntrl,
+                   Narms = Nalt,
+                   a1 = a1,
+                   reward = reward,
+                   Nparam = Nparam
+                   )
+        
+#fit stan model   
+rl_fit<- stan(file = "Hierarchical_cov_matrix_cholesky.stan", data=model_data, iter=2000,chains=6,cores =6) #iter - number of MCMC samples 
+
+print(rl_fit)
+rl_fit<-readRDS('fit.rds')
+
+
+# compare recovered parameters to true parameters  --------------------------------------------
+
+        
+#population level (hyperparameter)
+alpha_aux_mu_recovered   = (summary(rl_fit , pars=c("mu[1]"))$summary[,1])
+beta_aux_mu_recovered    = summary(rl_fit , pars=c("mu[2]"))$summary[,1]
+sigma_recovered          = matrix(summary(rl_fit , pars=c("sigma_matrix"))$summary[,1],2,2)
+omega_recovered          = cov2cor(sigma_matrix_recovered)
+
+tau_recovered            =summary(rl_fit , pars=c("tau"))$summary[,1]
+L_Omega_recovered        =matrix(summary(rl_fit , pars=c("L_Omega"))$summary[,1],2,2)
+omega=diag(tau_recovered)*L_Omega_recovered*t(L_Omega_recovered)
+omega*diag(tau_recovered)
+cov2cor(omega*diag(tau_recovered))
+
+
+
+
+#compare recovered to true population parameters
+#location parameters
+cat(paste('true alpha population parm is',     alpha_mu,' sample mean is',    mean(true.parms[,1]),'and recovered is',          inv.logit(alpha_aux_mu_recovered)),
+    paste('true beta population parm is',      beta_mu,' sample mean is',     mean(true.parms[,2]),'and recovered is',          exp(beta_aux_mu_recovered)),
+    paste('true alpha aux population parm is', alpha_aux_mu,' sample mean is',mean(auxiliary_parameters[,1]),'and recovered is',alpha_aux_mu_recovered),
+    paste('true beta aux population parm is',  beta_aux_mu,' sample mean is', mean(auxiliary_parameters[,2]),'and recovered is',beta_aux_mu_recovered),
+    sep = '\n')
+
+#scale parameters
+cat(paste('true alpha aux var parm is',        alpha_aux_var,' sample mean is',  var(auxiliary_parameters[,1]),'and recovered is',alpha_aux_var_recovered),
+    paste('true beta aux var parm is',         beta_aux_var,' sample mean is',   var(auxiliary_parameters[,2]),'and recovered is',beta_aux_var_recovered),
+    paste('true corr between aux parms is',    corr_alpha_beta,' sample mean is',cor(auxiliary_parameters[,1],auxiliary_parameters[,2]),
+      'and recovered is',cov_alpha_beta_recovered/(sqrt(alpha_aux_var_recovered)*sqrt(beta_aux_var_recovered))),
+    sep = '\n')
+
+
+#individual level parameters (subjects parameters)
+alpha_individual_recovered=summary(rl_fit , pars=c("alpha"))$summary[,1] 
+beta_individual_recovered=summary(rl_fit , pars=c("beta"))$summary[,1]
+plot(true.parms[,1],(alpha_individual_recovered))
+plot(true.parms[,2],(beta_individual_recovered))
+cor(true.parms,cbind(alpha_individual_recovered,beta_individual_recovered))
+
+#additional
+summary(rl_fit, pars=c("L_Omega"))$summary
+sigma_matrix_recovered=summary(rl_fit, pars=c("sigma_matrix"))$summary[,1]
+
+
+cor(true.parms[,1],true.parms[,2])
+cor(alpha_recovered,beta_recovered)
+var(true.parms[,1])
+var(alpha_recovered)
+var(true.parms[,2])
+var(beta_recovered)
+
+
+
+##2nd version
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write=TRUE)
+
+my_model<- stan_model(file = "rl_basic.stan") 
+sample <- sampling(object = my_model, data = model_data)
+
+fit <-optimizing(object = my_model, data = model_data)
+#c(fit$par[1],fit$par[2])
+
+
+
+my_model<- stan_model(file = "rl_basic.stan") 
+sample <- sampling(object = my_model, data = model_data)
+
+plot(sample, plotfun = "hist", pars= "alpha")
+plot(sample, plotfun = "hist", pars= "beta")
+
+library("shinystan")
+launch_shinystan(rl_fit)
+
+#calculate cor between true and recovered params
+df.tbl   <-lapply(1:length(Nalt), function(alt) {
+  lapply(1:length(Ntrl), function(trl) {
+    data.frame(Nalt=Nalt[alt],
+               Ntrl=Ntrl[trl],
+               cor.alpha=cor(true.parms$alpha,inv.logit((do.call(rbind,alpha[[alt]][[trl]])))),
+               cor.beta=cor(true.parms$beta,exp((do.call(rbind,beta[[alt]][[trl]])))))
+  })})
+
+df.tbl<-do.call(rbind,lapply(1:length(Nalt), function(alt) {do.call(rbind,df.tbl[[alt]])}))
+
+#print table to file
+df.tbl %>%
+  kable() %>%
+  kable_styling(bootstrap_options = c("striped", "hover", "condensed"))        
+
+
+
+
+
---
+++ modeling/sim_functions/simme_tsp.R
@@ -0,0 +1,102 @@
+
+
+#sim_1 helper function
+sim.block = function(par,cfg){
+  Ntrl          = cfg$Ntrl
+  Nalt          = cfg$Nalt
+  Noffer        = cfg$Noffer # how many cards from the deck are presented
+  rndwlk_frac   = cfg$rndwlk_frac
+  rndwlk_teacher= cfg$rndwlk_teacher
+  teacher.rate  = cfg$teacher.rate 
+
+  alpha.free             = inv.logit(par['alpha.free'])
+  alpha.inst.follow      = inv.logit(par['alpha.inst.follow'])
+  alpha.inst.oppose      = inv.logit(par['alpha.inst.oppose'])
+  alpha.teacher_as_bandit= inv.logit(par['alpha.teacher_as_bandit'])
+  alpha.reliability      = inv.logit(par['alpha.reliability'])
+  beta                   = exp(par['beta'])
+  inst.bias              = par['inst.bias']
+
+  Qcard         = rep(0, Nalt)
+  Rcard         = 0
+  Qteacher      = c(0,0) #follow or oppose
+  Rteacher      = 1
+  df            =data.frame()
+  for (t in 1:Ntrl){
+
+    #computer decides which cards to offer
+    raffle      = sample(1:Nalt,Noffer,prob=rep(1/Nalt,Nalt))
+    cards.expval= rndwlk_frac[raffle,t]
+    teacher.beta= rndwlk_teacher[t]
+ 
+    #teacher card and revel choices
+    teacher.ch      = sample(raffle,1,prob=exp(teacher.beta*cards.expval) / sum(exp(teacher.beta*cards.expval)))
+    teacher.reveal  = sample(c(0,1),1,prob=c((1-teacher.rate),teacher.rate))
+
+
+    #student integrated action values
+    Qnet=c(0,0)
+    Qnet                     = Rcard*Qcard[raffle]
+    Qnet[teacher.ch!=raffle] = Qnet[teacher.ch!=raffle]+Rteacher*Qteacher[1]
+    Qnet[teacher.ch==raffle] = Qnet[teacher.ch==raffle]+Rteacher*Qteacher[2]+(1-Rcard)*(1-Rteacher)*inst.bias
+    
+    #student choice
+    student.ch    = sample(raffle, size=1, replace=TRUE,prob=exp(beta*Qnet)/sum(exp(beta*Qnet)))
+    student.follow=(student.ch==teacher.ch)*1  
+    
+    #outcome
+    reward = sample(0:1, size=1, replace=TRUE,prob=c((1-rndwlk_frac[student.ch,t]),rndwlk_frac[student.ch,t]))
+    
+    #Prediction erros
+    PEcard             = reward-Qcard[student.ch]
+    PEteacher                  = reward-Qteacher[student.follow+1]
+    
+    #save data
+    df<-rbind(df,data.frame(
+      subject        =cfg$subject,
+      trial          =t,
+      offer1         =raffle[1],
+      offer2         =raffle[2],
+      ev1            =rndwlk_frac[1,t],
+      ev2            =rndwlk_frac[2,t],
+      ev3            =rndwlk_frac[3,t],
+      ev4            =rndwlk_frac[4,t],
+      teacher.ch     =teacher.ch,
+      teacher.reveal =teacher.reveal,
+      student.ch     =student.ch,
+      student.follow =student.follow,
+      reward         =reward,
+      PEcard         =PEcard,
+      PEteacher      =PEteacher,
+      Qcard1         =Qcard[1],
+      Qcard2         =Qcard[2],
+      Qcard3         =Qcard[3],
+      Qcard4         =Qcard[4],
+      Qteacher.opp   =Qteacher[1],
+      Qteacher.follow=Qteacher[2],
+      Rcard          =as.numeric(Rcard),
+      Rteacher       =as.numeric(Rteacher))
+    )
+    
+    #Card update student state-action values and reliability scores
+    alpha.student=alpha.inst.follow*(teacher.reveal*student.follow)     +
+                  alpha.inst.oppose*(teacher.reveal*(1-student.follow)) +
+                  alpha.free*       (1-teacher.reveal) 
+    
+    Qcard[student.ch]  = Qcard[student.ch]+alpha.student*PEcard
+    Rcard              = Rcard+alpha.reliability*((1-abs(PEcard))-Rcard)
+    
+    
+    #teacher update student state-action values and reliability scores
+    alpha.teacher      = alpha.teacher_as_bandit*teacher.reveal
+    Qteacher[student.follow+1] = Qteacher[student.follow+1] +alpha.teacher*PEteacher
+    Rteacher                   = Rteacher+alpha.reliability*((1-abs(PEteacher)-Rteacher))*teacher.reveal
+    
+  }
+  
+  return (df)
+}
+
+
+
+
