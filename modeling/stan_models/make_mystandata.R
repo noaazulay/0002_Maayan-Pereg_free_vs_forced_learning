ca0f1db4ed542300212fba7f517a69a23bb59b47
NitzanShahar <68427888+NitzanShahar@users.noreply.github.com>
2021-07-01 18:57:22 +0300

    fixing make stan data

--- modeling/stan_models/make_mystandata.R
+++ modeling/stan_models/make_mystandata.R
@@ -2,12 +2,21 @@
 #     These matrices are padded with Inf value to have exactly the same number of trials despite missing values
 #contributors: Nitzan Shahar, Shira Niv
 #inputs:
+#data - is your data frame
+#subject_column - is the specific vector of subjects from you data frame (e.g., data$subjectID)
 #Ntrials_per_subject - is a vector 1 x Nsubj with each cell including the number of available trials per subject
 #subjects_list - is a vector with the subjects id
 #var_toinclude - is a vector of strings with the names of data columns that should be included in the stan data
-#Nparms - is the number of parameters the model is going to have (to create the mu and sigma population parameters)
+#var_tobenamed - is a vector of strings for how to variables should be names according to stan model (in case the data and stan has different names for the same var)
 
-make_mystandata<-function(data, Ntrials_per_subject, subjects_list,var_toinclude,Nparams){
+make_mystandata<-function(data, subject_column,var_toinclude,var_tobenamed){
+  
+  #create subjects list (only unique values)
+  subjects_list      =unique(subject_column)
+  
+  #create an Ntrials_per_subject vector showing the number of trials for each subject
+  
+  Ntrials_per_subject=sapply(1:length(subjects_list), function(i) {sum(subject_column==subjects_list[i])})
   
   #find the largest number of available data per subject
   max_trials_per_subject=max(Ntrials_per_subject)
@@ -15,22 +24,22 @@ make_mystandata<-function(data, Ntrials_per_subject, subjects_list,var_toinclude
   #loop over the variables that needs to be included
   mydata<-lapply(var_toinclude,function(myvar) { 
     
-  #for each variable, loop over all subjects to create a padded matrix
-              t(sapply(subjects_list,function(subject) 
+    #for each variable, loop over all subjects to create a padded matrix
+    t(sapply(subjects_list,function(subject) 
       
-                      { #create vector for a specific variable and subject
-                        current_var=df[df$subject==subject,myvar]
-                        # data padding with Inf according to the max number of trials across subjects
-                        c(current_var,rep(9999,max_trials_per_subject-sum(df$subject==subject)))})) 
+    { #create vector for a specific variable and subject
+      current_var=data[subject_column==subject,myvar]
+      # data padding with Inf according to the max number of trials across subjects
+      c(current_var,rep(9999,max_trials_per_subject-sum(subject_column==subject)))})) 
     
   }
   )
   #add variables names
-  names(mydata)=var_toinclude
+  names(mydata)=var_tobenamed
   
   #add additional variables
   mydata=append(list(Nsubjects=length(subjects_list), 
                      Ntrials=max_trials_per_subject,  
                      Ntrials_per_subject=Ntrials_per_subject),
-                     mydata)
+                mydata)
 }
