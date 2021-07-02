da9778d6c9940106540b24ac655afc2079fe645d
NitzanShahar <68427888+NitzanShahar@users.noreply.github.com>
2021-07-02 17:31:56 +0300

    fixing a bug in the teacher choice coding

--- modeling/model_fitting_alpha_beta_bias_Qdiffcard_Qdiffteacher.R
+++ modeling/model_fitting_alpha_beta_bias_Qdiffcard_Qdiffteacher.R
@@ -18,11 +18,12 @@ load(paste('modeling/data/tab.Rdata',sep=""))
 tab$ch=tab$ch+1
 tab$frcA=tab$frcA+1
 tab$frcB=tab$frcB+1
+tab$teacher_choice=tab$teacher_choice+1
 
 tab$raffle_student_ch=(tab$ch==tab$frcB)*1+1
 tab$raffle_teacher_ch=(tab$teacher_choice==tab$frcB)*1+1
 names(tab)
-tab=tab[tab$session=='session1',]
+
 head(tab[,c(
   'ch',
   'rw',
@@ -67,18 +68,15 @@ start_time <- Sys.time()
 rl_fit<- stan(file = "modeling/stan_models/stan_alpha_beta_bias.stan", 
               data=data_for_stan, 
               iter=2000,                          #number of warmup=0.5*iter
-              chains=2,
-              cores =2, 
-              pars=c('mu'), #define which parameters to save so that the final file won't be larger then necessary
+              chains=4,
+              cores =4, 
+              #pars=c('mu'), #define which parameters to save so that the final file won't be larger then necessary
               save_warmup=F)
 
 end_time <- Sys.time()
 
 #save results
-saveRDS(rl_fit,file='modeling/results/Parameter_recovery_alpha_beta_bias_iqcorr.rds')
-trueparams=list(Nsubjects,mu,tau,cov_param,sigma,auxiliary_parameters)
-
-save(trueparams,file='modeling/results/Parameter_recovery_alpha_beta_bias_iqcorr_trueparams.Rdata')
+saveRDS(rl_fit,file='modeling/results/model_fit_alpha_beta_bias.rds')
 
 
 # examine mcmc ----------------------------------------------------------------------------
@@ -87,17 +85,20 @@ library(ggplot2)
 #rl_fit<-readRDS('modeling/results/Parameter_recovery_alpha_beta_bias_iqcorr.rds')
 #plot posteriors
 plot_title <- ggtitle("Posterior distributions",
-                      "with medians and 80% intervals")
+                      "with medians and 95% intervals")
 mcmc_areas(rl_fit,
-           pars = c("mu[3]"),
-           prob = 0.8) + plot_title
-
+           pars = c("mu[2]"),
+           prob = 0.95) + plot_title
+inv.logit(-2)
 
 
 #plot mcmc chains
 color_scheme_set("blue")
-mcmc_trace(rl_fit, pars = c("mu[1]", "mu[2]","mu[3]","mu[4]"), n_warmup=0,
+mcmc_trace(rl_fit, pars = c("mu[1]", "mu[2]","mu[3]"), n_warmup=0,
            facet_args = list(ncol = 1, strip.position = "left"))
 
 
 traceplot(rl_fit, c("mu[1]", "mu[2]","mu[3]"), inc_warmup = TRUE, nrow = 3)
+
+
+hist(summary(rl_fit , pars=c("bias"))$summary[,1])
